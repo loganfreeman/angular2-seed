cc31e85d3cdfe3f7b748fd38cff2b110972910c4
shanhong cheng <shnahongcheng@gmail.com>
2016-05-14 22:33:33 -0600

    border style

--- src/client/app/+sudoku/components/tile.component.ts
+++ src/client/app/+sudoku/components/tile.component.ts
@@ -7,4 +7,28 @@ import {Component, Input} from '@angular/core';
 })
 export class TileComponent {
   @Input() tile: any;
+
+  isThirdRow():boolean {
+    if(this.tile){
+      for(var i of [2, 5, 8]){
+        if( this.tile.get('id') >= i*9 && this.tile.get('id') < (i+1)*9 ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  isThirdCol():boolean {
+    if(this.tile){
+      for(var i of [2, 5, 8]){
+        for(var j = 0; j< 9; j++){
+          if(this.tile.get('id') == j*9 +i) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
 }
