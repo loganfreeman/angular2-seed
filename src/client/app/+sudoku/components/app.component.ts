7a40e0d507255e1624dffc1b0a0491168c6f661c
shanhong cheng <shnahongcheng@gmail.com>
2016-05-16 17:54:37 -0600

    range

--- src/client/app/+sudoku/components/app.component.ts
+++ src/client/app/+sudoku/components/app.component.ts
@@ -22,7 +22,7 @@ export class SudokuAppComponent {
   tileJson: string;
   modalSelected: number;
   selected: number;
-  items: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  items: number[][] = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
   animationsEnabled: boolean = true;
 
   @ViewChild('myModal')
