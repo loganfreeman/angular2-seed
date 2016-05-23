1270a3660b07f27d97f6fbc67d78adcfee75f82e
shanhong cheng <shanhong.cheng@utah.edu>
2016-05-23 16:29:58 -0600

    ngClass ngStyle

--- src/client/app/+tetris/components/app.component.ts
+++ src/client/app/+tetris/components/app.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild } from '@angular/core';
 
 import { MODAL_DIRECTIVES, ModalResult, ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
 
+import {GridService} from './grid'
+
+import {GameData} from './game-data';
 
 
 @Component({
@@ -11,5 +14,45 @@ import { MODAL_DIRECTIVES, ModalResult, ModalComponent} from 'ng2-bs3-modal/ng2-
   directives: [MODAL_DIRECTIVES]
 })
 export class TetrisComponent {
+  getGridService(){
+    return GridService.grid;
+  }
 
+  getFilledClass(cell:any) {
+      var pieceClass = '';
+      if (cell.filled) {
+          switch(cell.shape) {
+              case 0: pieceClass = 'dy-L-filled';
+                  break;
+              case 1: pieceClass = 'dy-O-filled';
+                  break;
+              case 2: pieceClass = 'dy-I-filled';
+                  break;
+              case 3: pieceClass = 'dy-T-filled';
+                  break;
+              case 4: pieceClass = 'dy-J-filled';
+                  break;
+              case 5: pieceClass = 'dy-S-filled';
+                  break;
+              case 6: pieceClass = 'dy-Z-filled';
+                  break;
+              default: pieceClass = 'dy-X-filled';
+                  break;
+          }
+      }
+      if (cell.ghost) {
+          pieceClass += (pieceClass.length > 0) ? ' dy-ghost-piece' : 'dy-ghost-piece';
+      }
+      return pieceClass;
+  }
+
+  getFilledCustomColor(cell:any):any {
+      if (cell.filled && cell.shape === 7) {
+          return {
+              'background-color': GameData.getColor()
+          };
+      }else{
+        return {}
+      }
+  }
 }
