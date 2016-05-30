07fc9331c10d043815c703f34f9ae692131c58eb
shanhong cheng <shnahongcheng@gmail.com>
2016-05-30 10:48:20 -0600

    clean up

--- src/client/app/+home/components/home.component.ts
+++ src/client/app/+home/components/home.component.ts
@@ -10,16 +10,4 @@ import { NameListService } from '../../shared/index';
   directives: [FORM_DIRECTIVES]
 })
 export class HomeComponent {
-  newName: string;
-  constructor(public nameListService: NameListService) {}
-
-  /*
-   * @param newname  any text as input.
-   * @returns return false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    this.nameListService.add(this.newName);
-    this.newName = '';
-    return false;
-  }
 }
