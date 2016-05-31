31a8b9c3777ab248678ff1c59bd02c3fd8ed68d6
shanhong cheng <shnahongcheng@gmail.com>
2016-05-30 21:08:22 -0600

    clean up

--- src/client/app/+home/components/home.component.spec.ts
+++ src/client/app/+home/components/home.component.spec.ts
@@ -21,29 +21,13 @@ export function main() {
 
             let homeInstance = rootTC.debugElement.children[0].componentInstance;
             let homeDOMEl = rootTC.debugElement.children[0].nativeElement;
-            let nameListLen = function () {
-              return homeInstance.nameListService.names.length;
-            };
-
-            expect(homeInstance.nameListService).toEqual(jasmine.any(NameListService));
-            expect(nameListLen()).toEqual(4);
-	    expect(getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-
-            homeInstance.newName = 'Minko';
-            homeInstance.addName();
-            rootTC.detectChanges();
-
-            expect(nameListLen()).toEqual(5);
-	    expect(getDOM().querySelectorAll(homeDOMEl, 'li').length).toEqual(nameListLen());
-
-	    expect(getDOM().querySelectorAll(homeDOMEl, 'li')[4].textContent).toEqual('Minko');
           });
       }));
   });
 }
 
 @Component({
-  providers: [NameListService],
+  providers: [],
   selector: 'test-cmp',
   template: '<sd-home></sd-home>',
   directives: [HomeComponent]
