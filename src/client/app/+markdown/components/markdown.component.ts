518523dc8ac68bc9c6022e833189b27d496d36b9
shanhong cheng <shnahongcheng@gmail.com>
2016-06-24 18:56:52 -0600

    jQuery

--- src/client/app/+markdown/components/markdown.component.ts
+++ src/client/app/+markdown/components/markdown.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var jQuery:any;
 @Component({
   selector: 'sd-markdown',
   templateUrl: 'app/+markdown/components/markdown.component.html',
   styleUrls: ['app/+markdown/components/markdown.component.css'],
 })
-export class MarkdownComponent {
-
+export class MarkdownComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    jQuery('.selectpicker').selectpicker({
+      style: 'btn-info',
+      size: 4
+    });
+  }
 }
